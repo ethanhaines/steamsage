3439dff19a69eba6167088c2f8cb688de448f0c6
ethanhaines <122860615+ethanhaines@users.noreply.github.com>
2024-11-22 19:41:32 -0500

    Working on setting up supabase

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,19 @@
 //
 // Created by eboyh on 11/22/2024.
 //
+
+#include <iostream>
+#include <libpq-fe.h>
+int main() {
+    std::cout << "Enter password: " << std::endl;
+    std::string pw;
+    std::cin >> pw;
+    std::cout << std::endl;
+    std::string connect = "postgresql://postgres.eyjgqtoxgvibvauumhuw:" + pw + "@aws-0-us-east-1.pooler.supabase.com:6543/postgres";
+    PGconn *conn = PQconnectdb(connect.c_str());
+    if (PQstatus(conn) == CONNECTION_OK) {
+        std::cout << "Connected" << std::endl;
+    }
+
+    return 0;
+}
