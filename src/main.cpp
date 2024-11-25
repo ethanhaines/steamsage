e3286943187b02fddc3b48420b662ae1c43431e4
ethanhaines <eboyhaines@gmail.com>
2024-11-25 16:55:39 -0500

    Swapped to csv parsing
    The database route seemed a bit too annoying to deal with, decided on just parsing a csv provided

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,6 @@
 #include <iostream>
 #include <C:\Program Files\PostgreSQL\17\include\libpq-fe.h>
 int main() {
-    std::cout << "Enter password: " << std::endl;
-    std::string pw;
-    std::cin >> pw;
-    std::cout << std::endl;
-    std::string connect = "postgresql://postgres.eyjgqtoxgvibvauumhuw:" + pw + "@aws-0-us-east-1.pooler.supabase.com:6543/postgres";
-    PGconn *conn = PQconnectdb(connect.c_str());
-    if (PQstatus(conn) == CONNECTION_OK) {
-        std::cout << "Connected" << std::endl;
-    }
 
     return 0;
 }
