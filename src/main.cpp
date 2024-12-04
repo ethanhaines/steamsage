8d2c7e5c3d7907c10971aaec992b2d6fafcdf9a8
ethanhaines <122860615+ethanhaines@users.noreply.github.com>
2024-12-03 22:01:28 -0500

    edited main and deleted BFS redundant file

--- src/main.cpp
+++ src/main.cpp
@@ -21,15 +21,12 @@ int main() {
     AdjList graph;
 
     std::unordered_map<std::string, std::vector<std::string>> games = parser.parse(); // this contains every game in our csv
-    std::cout << games.size() << std::endl; // (matching tag count + matching tag count) / (total number of the tags between the two games)
     std::cout << "Enter a game you like: (for now this has to be exact match but later we will use a drop down search list): " << std::endl;
     std::string game;
-    std::cout << games.size() << std::endl;
     std::getline(std::cin, game);
     graph.initialize_graph(game, games[game], games, -1);
     graph.print();
     std::cout << graph.get_size() << std::endl;
-    //probably want to start using modularity and creating functions for everything, like init() for parsing
 
     return 0;
 }
